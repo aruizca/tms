6317f72544a9db304702cef0637e41e9bf22d06f
Charles Gretton <charles.gretton@nicta.com.au>
2014-09-11 11:49:55 +1000

    New API for control

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -2,6 +2,128 @@ var express = require('express');
 var router = express.Router();
 var twitterService = require('../services/twitterService');
 
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+CollectionDriver = require('./collectionDriver').CollectionDriver;
+
+var mongoHost = 'localHost';
+var mongoPort = 27017; 
+var collectionDriver;
+ 
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); 
+mongoClient.open(function(err, mongoClient) { 
+  if (!mongoClient) {
+      console.error("Error! Exiting... Must start MongoDB first");
+      process.exit(1); 
+  }
+  var db = mongoClient.db("canberratedx2014straightcsv"); 
+  collectionDriver = new CollectionDriver(db); 
+});
+
+
+
+router.get('static/:collection/:entity', function(req, res) {
+   var params = req.params;
+   var entity = params.entity;
+   var collection = params.collection;
+   if (entity) {
+       collectionDriver.get(collection, entity, function(error, objs) {
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } 
+       });
+   } else {
+      res.send(400, {error: 'bad url', url: req.url});
+   }
+});
+
+router.get('static/:collection', function(req, res) { 
+   var params = req.params;
+   collectionDriver.findAll(req.params.collection, function(error, objs) { 
+    	  if (error) { res.send(400, error); }
+	      else { 
+	          if (req.accepts('html')) {
+    	          res.render('data',{objects: objs, collection: req.params.collection}); 
+              } else {
+	          res.set('Content-Type','application/json'); 
+                  res.send(200, objs); 
+              }
+         }
+   	});
+});
+ 
+
+
+
+router.post('static/:collection', function(req, res) {
+    var object = req.body;
+    var collection = req.params.collection;
+    collectionDriver.save(collection, object, function(err,docs) {
+          if (err) { res.send(400, err); } 
+          else { res.send(201, docs); } 
+     });
+});
+
+//update a specific object
+CollectionDriver.prototype.update = function(collectionName, obj, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) {
+        if (error) callback(error);
+        else {
+            obj._id = ObjectID(entityId); 
+            obj.updated_at = new Date(); 
+            the_collection.save(obj, function(error,doc) { 
+                if (error) callback(error);
+                else callback(null, obj);
+            });
+        }
+    });
+};
+
+//delete a specific object
+CollectionDriver.prototype.delete = function(collectionName, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { 
+        if (error) callback(error);
+        else {
+            the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc) { 
+                if (error) callback(error);
+                else callback(null, doc);
+            });
+        }
+    });
+};
+
+router.put('static/:collection/:entity', function(req, res) { 
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+       collectionDriver.update(collection, req.body, entity, function(error, objs) { 
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); }
+       });
+   } else {
+       var error = { "message" : "Cannot PUT a whole collection" };
+       res.send(400, error);
+   }
+});
+
+router.delete('static/:collection/:entity', function(req, res) { 
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+       collectionDriver.delete(collection, entity, function(error, objs) { 
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } 
+       });
+   } else {
+       var error = { "message" : "Cannot DELETE a whole collection" };
+       res.send(400, error);
+   }
+});
+
+
+
+
 router.get('/last-tweets/:username/:numTweets', function(req, res){
     var username = req.params.username ? req.params.username : '';
     var numTweets = req.params.numTweets ? req.params.numTweets : 10;
