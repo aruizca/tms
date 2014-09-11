90be66b8c585d2e98aa939cc8c632099c9c18d05
Angel Ruiz <angel.ruiz@nicta.com.au>
2014-09-11 16:44:48 +1000

    - Adds scaffolding for registration upload feature

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var registrationService = require('../services/registrationService');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('home', { title: 'SMMS: Social Media Monitoring Service' });
 });
 
-router.get('/rest-api-example', function(req, res) {
-    res.render('rest-api-example', { title: 'REST API example' });
+router.get('/registration', function(req, res) {
+    res.render('registration', { title: 'Registration Upload Form' });
 });
 
-router.get('/streaming-api-example', function(req, res) {
-    res.render('streaming-api-example', { title: 'STREAMING API example' });
+router.post('/registration/upload', function(req, res) {
+    // TODO Use https://github.com/andrewrk/node-multiparty/ to get the file
+    // TODO Send file to registrationService
 });
 
-router.get('/streaming-api-example-2', function(req, res) {
-    res.render('streaming-api-example-2', { title: 'STREAMING API example 2' });
-});
 
 module.exports = router;
---
+++ server/services/registrationService.js
@@ -0,0 +1,1 @@
+// TODO create method to parse the excel (https://www.npmjs.org/package/excel-parser) and save each row in collection 'registration' in MongoDB
