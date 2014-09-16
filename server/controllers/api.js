ab4c703f14c5b904ede92d8ddc28c94e988b91ee
Angel Ruiz <angel.ruiz@nicta.com.au>
2014-09-16 11:02:02 +1000

    Fixed TweetRedux template when there is media available Added rest service /api/tweets/by-screen-name/screen_name using JSONP

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,21 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var S = require('string');
 var twitterService = require('../services/twitterService');
 
-router.get('/last-tweets/:username/:numTweets', function(req, res){
-    var username = req.params.username ? req.params.username : '';
-    var numTweets = req.params.numTweets ? req.params.numTweets : 10;
-    twitterService.getLastTweetsFromUser(username, numTweets, function(err, data) {
-        res.end(data);
-    });
+router.get('/tweets/by-pin/:userId', function(req, res){
+    var userId = req.params.userId;
+    if (userId) {
+        // TODO
+    }
 });
 
-router.get('/follows/:username', function(req, res){
-    var username = req.params.username ? req.params.username : '';
-    twitterService.getFollows(username, function(err, data) {
-        res.end(data);
-    });
+router.get('/tweets/by-screen-name/:screenName', function(req, res){
+    var screenName = req.params.screenName;
+    if (screenName) {
+        twitterService.getTweetsByScreenName(screenName, res, function(tweets, res) {
+            res.jsonp(tweets);
+        })
+    }
 });
 
+router.get('/tweets/anonymous', function(req, res){
+        // TODO
+
+});
 
 module.exports = router;
--- server/services/twitterService.js
+++ server/services/twitterService.js
@@ -1,13 +1,15 @@
 var tweetStream = require('node-tweet-stream');
 var cfg = require('../../app-config.json');
 var render = require('render');
-var handlebars = require("handlebars");
-var fs = require("fs");
+var handlebars = require('handlebars');
+var fs = require('fs');
 var path = require('path');
 var debug = require('debug')('smms');
+var _ = require('underscore');
+var S = require('string');
 
 // Reduced templates
-var tweetReduxTemplate = fs.readFileSync('server/templates/tweetRedux.hbs', "utf8");
+var tweetReduxTemplate = fs.readFileSync('server/templates/tweetRedux.hbs', 'utf8');
 var tweetJsonBuilder = handlebars.compile(tweetReduxTemplate);
 
 // Real Time tracking Status
@@ -29,10 +31,11 @@ mongoFactory.getConnection(cfg.mongodb.url).then(function(dbConnection) {
  * @param io Socket.io instance
  */
 var initTwitterWebSocketServices = function(app, io) {
+
     io.on('connection', function (socket) {
         var tweetStreamClient = new tweetStream(cfg['node-tweet-stream']);
 
-        // "client:start" event handling
+        // 'client:start' event handling
         socket.on('client:start', function (keywords) {
             // Status management
             status.running = true;
@@ -48,17 +51,21 @@ var initTwitterWebSocketServices = function(app, io) {
             tweetStreamClient.on('tweet', function (tweet) {
                 try {
                     // Store the tweet in the DB
-                    db.collection("tweets").insert(tweet, function (err) {
+                    db.collection('tweets').insert(tweet, function (err) {
 
                         // Generate redux json
-                        var tweetRedux = tweetJsonBuilder(tweet);
+                        var tweetRedux = tweetJsonBuilder(cleanTweetText(tweet));
                         if (app.get('env') === 'development') {
                             console.log(tweetRedux);
                         }
 
                         // Send resulting html to all connected clients
                         if (status.running) {
-                            io.emit('server:tweet', JSON.stringify(tweetRedux));
+                            try {
+                                io.emit('server:tweet', JSON.parse(tweetRedux));
+                            } catch (ex) {
+                                debug(ex);
+                            }
                         }
                     });
                 } catch (e) {
@@ -67,19 +74,53 @@ var initTwitterWebSocketServices = function(app, io) {
             });
         });
 
-        // "client:stop" event handling
+        // 'client:stop' event handling
         socket.on('client:stop', function () {
             // Status management
             status.running = false;
             io.emit('server:status-update', status);
         });
 
-        // "client:stop" event handling
+        // 'client:stop' event handling
         socket.on('client:status-report', function () {
             io.emit('server:status-update', status);
         });
     });
 };
 
+/**
+ *
+ * @param screenName
+ * @param res
+ * @param callback
+ */
+var getTweetsByScreenName = function(screenName, res, callback) {
+    db.collection('tweets').find({'user.screen_name': screenName}).toArray(function(err, tweets) {
+
+        callback(_.map(tweets, function(tweet) {
+            try {
+                return JSON.parse(tweetJsonBuilder(cleanTweetText(tweet)));
+            } catch (ex) {
+                debug(ex);
+            }
+        }), res);
+    });
+};
+
+/**
+ * This is to prevent weird twiits with carrier return from breaking
+ * When you apply JSON stringify to a single String it surrounds the string with quotes that need to be removed.
+ * @param tweet
+ * @returns {tweet}
+ */
+var cleanTweetText = function (tweet) {
+    tweet.text = S(JSON.stringify(tweet.text.replace("\"", ''))).chompLeft("\"").chompRight("\"").s;
+    if (tweet.retweeted_status) {
+        tweet.retweeted_status.text = S(JSON.stringify(tweet.retweeted_status.text)).chompLeft("\"").chompRight("\"").s;
+    }
+    return tweet;
+};
+
 exports.initTwitterWebSocketServices = initTwitterWebSocketServices;
+exports.getTweetsByScreenName = getTweetsByScreenName;
 
