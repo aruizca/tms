fd5ee41cbe909e136589ea916f461d72dbaacdda
Angel Ruiz <angel.ruiz@nicta.com.au>
2014-09-16 17:30:16 +1000

    Merged with Charles code FINAL

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -4,175 +4,55 @@ var S = require('string');
 var twitterService = require('../services/twitterService');
 var questionaireService = require('../services/questionaireService');
 
-
-MongoClient = require('mongodb').MongoClient,
-Server = require('mongodb').Server,
-CollectionDriver = require('./collectionDriver').CollectionDriver;
-
-var mongoHost = 'localHost';
-var mongoPort = 27017; 
-var collectionDriver;
- 
-var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); 
-mongoClient.open(function(err, mongoClient) { 
-  if (!mongoClient) {
-      console.error("Error! Exiting... Must start MongoDB first");
-      process.exit(1); 
-  }
-  var db = mongoClient.db("canberratedx2014straightcsv"); 
-  collectionDriver = new CollectionDriver(db); 
-});
-
-
-
-router.get('static/:collection/:entity', function(req, res) {
-   var params = req.params;
-   var entity = params.entity;
-   var collection = params.collection;
-   if (entity) {
-       collectionDriver.get(collection, entity, function(error, objs) {
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } 
-       });
-   } else {
-      res.send(400, {error: 'bad url', url: req.url});
-   }
-});
-
-router.get('static/:collection', function(req, res) { 
-   var params = req.params;
-   collectionDriver.findAll(req.params.collection, function(error, objs) { 
-    	  if (error) { res.send(400, error); }
-	      else { 
-	          if (req.accepts('html')) {
-    	          res.render('data',{objects: objs, collection: req.params.collection}); 
-              } else {
-	          res.set('Content-Type','application/json'); 
-                  res.send(200, objs); 
-              }
-         }
-   	});
-});
- 
-
-
-
-router.post('static/:collection', function(req, res) {
-    var object = req.body;
-    var collection = req.params.collection;
-    collectionDriver.save(collection, object, function(err,docs) {
-          if (err) { res.send(400, err); } 
-          else { res.send(201, docs); } 
-     });
-});
-
-//update a specific object
-CollectionDriver.prototype.update = function(collectionName, obj, entityId, callback) {
-    this.getCollection(collectionName, function(error, the_collection) {
-        if (error) callback(error);
-        else {
-            obj._id = ObjectID(entityId); 
-            obj.updated_at = new Date(); 
-            the_collection.save(obj, function(error,doc) { 
-                if (error) callback(error);
-                else callback(null, obj);
-            });
-        }
-    });
-};
-
-//delete a specific object
-CollectionDriver.prototype.delete = function(collectionName, entityId, callback) {
-    this.getCollection(collectionName, function(error, the_collection) { 
-        if (error) callback(error);
-        else {
-            the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc) { 
-                if (error) callback(error);
-                else callback(null, doc);
-            });
-        }
-    });
-};
-
-router.put('static/:collection/:entity', function(req, res) { 
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       collectionDriver.update(collection, req.body, entity, function(error, objs) { 
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); }
-       });
-   } else {
-       var error = { "message" : "Cannot PUT a whole collection" };
-       res.send(400, error);
-   }
-});
-
-router.delete('static/:collection/:entity', function(req, res) { 
-    var params = req.params;
-    var entity = params.entity;
-    var collection = params.collection;
-    if (entity) {
-       collectionDriver.delete(collection, entity, function(error, objs) { 
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } 
-       });
-   } else {
-       var error = { "message" : "Cannot DELETE a whole collection" };
-       res.send(400, error);
-   }
-});
-
-
-
-
-router.get('/last-tweets/:username/:numTweets', function(req, res){
-    var username = req.params.username ? req.params.username : '';
-    var numTweets = req.params.numTweets ? req.params.numTweets : 10;
-    twitterService.getLastTweetsFromUser(username, numTweets, function(err, data) {
-        res.end(data);
-    });
-});
-
-
-router.post('/mongodbcollectionquery/:collection', function(req, res) {
-    var object = req.body;
-    var collection = req.params.collection;
-    try {
-	questionaireService.addFileToCollection(collection, object, res, function (res) {
-	    console.log("In questionaire service callback.");
-        });
-    }catch (ex) {
-	console.log("Something went wrong in questionaire service callback.");
-    }
-});
-
-router.get('/mongodbcollectionquery/:collection/:entity', function(req, res) { 
-    try {
-	questionaireService.queryMongoDBCollectionEntity(req, res, function (res) {
-	    console.log("In questionaire service callback.");
-        });
-    }catch (ex) {
-	console.log("Something went wrong in questionaire service callback.");
-    }
-});
-
-router.get('/tweets/:screenName', function(req, res){
+router.get('/tweets/:screenName', function (req, res) {
     var screenName = req.params.screenName;
     if (screenName) {
-        twitterService.getTweetsByScreenName(screenName, res, function(tweets, res) {
+        twitterService.getTweetsByScreenName(screenName, res, function (tweets, res) {
             res.jsonp(tweets);
         })
     }
 });
 
-router.get('/tweets/number/:screenName', function(req, res){
+router.get('/tweets/number/:screenName', function (req, res) {
     var screenName = req.params.screenName;
     if (screenName) {
-        twitterService.getTweetsNumberByScreenName(screenName, res, function(tweetsNumber, res) {
+        twitterService.getTweetsNumberByScreenName(screenName, res, function (tweetsNumber, res) {
             res.jsonp(tweetsNumber);
         })
+    }
+});
+
+router.get('/mongodbcollectionquery/:collection', function (req, res) {
+    console.log("Starting questionaire service callback.");
+    try {
+        questionaireService.queryMongoDBCollection(req, res, function (res) {
+            console.log("In questionaire service callback.");
+        });
+    } catch (ex) {
+        console.log("Something went wrong in questionaire service callback.");
+    }
+});
+
+
+router.post('/mongodbcollectionquery/:collection', function (req, res) {
+    var object = req.body;
+    var collection = req.params.collection;
+    try {
+        questionaireService.addFileToCollection(collection, object, res, function (res) {
+            console.log("In questionaire service callback.");
+        });
+    } catch (ex) {
+        console.log("Something went wrong in questionaire service callback.");
+    }
+});
+
+router.get('/mongodbcollectionquery/:collection/:entity', function (req, res) {
+    try {
+        questionaireService.queryMongoDBCollectionEntity(req, res, function (res) {
+            console.log("In questionaire service callback.");
+        });
+    } catch (ex) {
+        console.log("Something went wrong in questionaire service callback.");
     }
 });
 
